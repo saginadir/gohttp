6ee694c9133a6427f2d7afc3a6d96fabca6d755a
sagy.nadir <sagy.nadir@alibaba-inc.com>
2018-04-28 20:44:45 +0800

    added examples

---
+++ gohttp.go
@@ -0,0 +1,77 @@
+package gohttp
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+)
+
+// Post is an example of http post request
+func Post() {
+	message := map[string]interface{}{
+		"hello": "world",
+		"life":  42,
+		"embedded": map[string]string{
+			"yes": "of course!",
+		},
+	}
+
+	bytesRepresentation, err := json.Marshal(message)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	resp, err := http.Post("https://httpbin.org/post", "application/json", bytes.NewBuffer(bytesRepresentation))
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	var result map[string]interface{}
+
+	json.NewDecoder(resp.Body).Decode(&result)
+
+	log.Println(result)
+	log.Println(result["data"])
+}
+
+// PostForm is an example of http post request
+func PostForm() {
+	formData := url.Values{
+		"name": {"masnun"},
+	}
+
+	resp, err := http.PostForm("https://httpbin.org/post", formData)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	var result map[string]interface{}
+
+	json.NewDecoder(resp.Body).Decode(&result)
+
+	log.Println(result["form"])
+}
+
+// Get is an example of http post request
+func Get() {
+	const url = "http://google.com"
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+		os.Exit(1)
+	}
+
+	b, err := ioutil.ReadAll(resp.Body)
+	resp.Body.Close()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+		os.Exit(1)
+	}
+	fmt.Printf("%s", b)
+}
+
